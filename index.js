a4de3dfdbc7de666548123f16343d2b325ef3fc8
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-12-12 18:42:57 +0200

    cleaned up the code

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const files = document.getElementById("root-folder-container");
 const contextMenu = document.createElement("div");
 contextMenu.id = "context-menu";
-const form = document.forms[0];
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (!this[0].value) {
-    return;
-  }
-  if (this[0].name === "filename") {
-    addFile(this[0].dataset.id, this[0].value);
-  } else {
-    addFolder(this[0].dataset.id, this[0].value);
-  }
-  hideForm();
-});
-
-const showForm = () => (form.style.visibility = "visible");
-const hideForm = () => {
-  form.style.visibility = "hidden";
-  form.reset();
-};
+function createForm(id, type) {
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.dataset.id = id;
+  form.append(input);
+  document.querySelector(`#${id} ul`).append(form);
+  form[0].focus();
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (type === "file") {
+      addFile(this[0].dataset.id, this[0].value);
+    } else if (type === "folder") {
+      addFolder(this[0].dataset.id, this[0].value);
+    }
+    form.reset();
+    form.remove();
+  });
+  form[0].addEventListener("blur", (e) => form.remove());
+}
 
 files.addEventListener("contextmenu", function (e) {
   e.preventDefault();
@@ -34,63 +35,60 @@ files.addEventListener("contextmenu", function (e) {
   }
 });
 
+//creating custom context menus for right clicking the files and folders
 function folderContextMenu(id, x, y) {
-  if (!contextMenu.childElementCount) {
-    form[0].dataset.id = id;
-    contextMenu.style.top = `${y}px`;
-    contextMenu.style.left = `${x}px`;
-    const newFile = document.createElement("div");
-    newFile.innerText = "New File";
-    newFile.addEventListener("click", (e) => {
-      form[0].name = "filename";
-      showForm();
-    });
-    const newFolder = document.createElement("div");
-    newFolder.innerText = "New Folder";
-    newFolder.addEventListener("click", (e) => {
-      form[0].name = "folder-name";
-      showForm();
-    });
-    const deleteFolder = document.createElement("div");
-    deleteFolder.innerText = "Delete";
-    deleteFolder.addEventListener("click", (e) => deleteEntry(id));
+  removeContextMenu();
 
-    contextMenu.append(newFile, newFolder);
-    if (id !== "root-folder") {
-      contextMenu.append(deleteFolder);
-    }
-    contextMenu
-      .querySelectorAll("div")
-      .forEach((el) => (el.className = "item"));
-    document.getElementById(id).append(contextMenu);
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.left = `${x}px`;
+  const newFile = document.createElement("div");
+  newFile.innerText = "New File";
+  newFile.addEventListener("click", (e) => {
+    createForm(id, "file");
+  });
+  const newFolder = document.createElement("div");
+  newFolder.innerText = "New Folder";
+  newFolder.addEventListener("click", (e) => {
+    createForm(id, "folder");
+  });
+  const deleteFolder = document.createElement("div");
+  deleteFolder.innerText = "Delete";
+  deleteFolder.addEventListener("click", (e) => deleteEntry(id));
+
+  contextMenu.append(newFile, newFolder);
+  if (id !== "root-folder") {
+    contextMenu.append(deleteFolder);
   }
+  contextMenu.querySelectorAll("div").forEach((el) => (el.className = "item"));
+  document.querySelector("body").append(contextMenu);
 }
 
 function fileContextMenu(id, x, y) {
-  if (!contextMenu.childElementCount) {
-    contextMenu.style.top = `${y}px`;
-    contextMenu.style.left = `${x}px`;
-    const deleteFile = document.createElement("div");
-    deleteFile.innerText = "Delete";
-    deleteFile.addEventListener("click", (e) => deleteEntry(id));
-    contextMenu.append(deleteFile);
-    contextMenu
-      .querySelectorAll("div")
-      .forEach((el) => (el.className = "item"));
-    document.getElementById(id).append(contextMenu);
+  removeContextMenu();
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.left = `${x}px`;
+  const deleteFile = document.createElement("div");
+  deleteFile.innerText = "Delete";
+  deleteFile.className = "item";
+  deleteFile.addEventListener("click", (e) => deleteEntry(id));
+  contextMenu.append(deleteFile);
+  document.querySelector("body").append(contextMenu);
+}
+
+//hiding the custom context menus or input on clicking somewhere else
+document.querySelector("body").addEventListener("click", hideContextMenu);
+
+function hideContextMenu(e) {
+  if (e.target.id !== "context-menu") {
+    removeContextMenu();
   }
 }
 
-document.querySelector("body").addEventListener("click", hideContextMenu);
-document.querySelector("body").addEventListener("contextmenu", hideContextMenu);
-
-function hideContextMenu(e) {
-  if (e.target.className !== "file" && e.target.className !== "folder") {
-    while (contextMenu.firstChild) {
-      contextMenu.removeChild(contextMenu.lastChild);
-    }
-    contextMenu.remove();
+function removeContextMenu() {
+  while (contextMenu.firstChild) {
+    contextMenu.removeChild(contextMenu.lastChild);
   }
+  contextMenu.remove();
 }
 
 function addFolder(id, folderName) {
@@ -116,11 +114,16 @@ function deleteEntry(id) {
   document.getElementById(id).remove();
 }
 
-document.querySelector(".folder").addEventListener("click", collapseFolder);
+document
+  .querySelector("#root-folder")
+  .addEventListener("click", collapseFolder);
 
 function collapseFolder(e) {
+  if (!e.target.id) {
+    return;
+  }
   const target = document.querySelector(`#${e.target.id} ul`);
-  if (target && target.style.display !== "none") {
+  if (target.innerHTML && target.style.display !== "none") {
     target.style.display = "none";
   } else target.style.display = "block";
 }
