bad22ab74957b8773158c0429076e7185b46aee5
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-12-14 02:03:58 +0200

    added texteditor.js

---
+++ client/text-editor.js
@@ -0,0 +1,42 @@
+const textEditor = document.createElement("pre");
+
+async function insertFiletoEditor(filePath) {
+  try {
+    const editorContainer = document.getElementById("text-editor-container");
+    if (editorContainer.childElementCount > 0) {
+      clearElementsChildren(editorContainer);
+    }
+    textEditor.contentEditable = true;
+    textEditor.className = "h-100";
+    const editorHeader = document.createElement("div");
+    editorHeader.className = "d-flex justify-content-between";
+    const p = document.createElement("p");
+    p.innerText = filePath.includes("/")
+      ? filePath.split("/").slice(-1)
+      : filePath;
+    const btn = document.createElement("button");
+    btn.className = "btn btn-primary";
+    btn.addEventListener("click", saveFileContents);
+    btn.innerText = "Save";
+    editorHeader.append(p, btn);
+
+    editorContainer.append(editorHeader, textEditor);
+    const fileContents = await getFileContents(filePath);
+    textEditor.innerHTML = (await fileContents.text())
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+    textEditor.dataset.path = filePath;
+    textEditor.focus();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function saveFileContents() {
+  try {
+    await addFileContents(
+      textEditor.dataset.path,
+      textEditor.innerHTML.replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+    );
+  } catch (error) {}
+}
