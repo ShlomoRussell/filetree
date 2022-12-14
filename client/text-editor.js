7a09f3e097e7c15f9896194eb908e9db688dcb12
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-12-14 17:48:45 +0200

    cleaned upand fixed text-editor.js

--- client/text-editor.js
+++ client/text-editor.js
@@ -1,46 +1,94 @@
+const editorContainer = document.getElementById("text-editor-container");
 const textEditor = document.createElement("pre");
+textEditor.style.background = "black";
+textEditor.style.color = "white";
+textEditor.contentEditable = true;
+textEditor.className = "h-50";
+const editorHeader = document.createElement("div");
+editorHeader.className = "d-flex justify-content-between mb-3";
+const p = document.createElement("p");
+const runBtn = document.createElement("button");
+runBtn.className = "btn btn-secondary";
+runBtn.addEventListener("click", runTerminal, { capture: true });
+runBtn.innerText = "Run ❯";
+const saveBtn = document.createElement("button");
+saveBtn.className = "btn btn-primary";
+saveBtn.addEventListener("click", saveFileContents, { capture: true });
+saveBtn.innerText = "Save";
+editorHeader.append(p, saveBtn, runBtn);
 
 /**
  *
  * @param {string} filePath
  */
 async function insertFiletoEditor(filePath) {
+  const filename = filePath.includes("/")
+    ? filePath.split("/").slice(-1).join()
+    : filePath;
+  if (p.innerText === filename) {
+    return;
+  }
   try {
-    const editorContainer = document.getElementById("text-editor-container");
-    if (editorContainer.childElementCount > 0) {
-      clearElementsChildren(editorContainer);
+    clearElementsChildren(editorContainer);
+    if (textEditor && editorHeader) {
+      clearElementsChildren(textEditor);
     }
-    textEditor.contentEditable = true;
-    textEditor.className = "h-100";
-    const editorHeader = document.createElement("div");
-    editorHeader.className = "d-flex justify-content-between";
-    const p = document.createElement("p");
-    p.innerText = filePath.includes("/")
-      ? filePath.split("/").slice(-1)
-      : filePath;
-    const btn = document.createElement("button");
-    btn.className = "btn btn-primary";
-    btn.addEventListener("click", saveFileContents);
-    btn.innerText = "Save";
-    editorHeader.append(p, btn);
-
-    editorContainer.append(editorHeader, textEditor);
-    const fileContents = await getFileContents(filePath);
-    textEditor.innerHTML = (await fileContents.text())
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;");
+    p.innerText = filename;
+    const fileContents = await getFileContents(filePath).then(
+      async (res) => await res.text()
+    );
+    textEditor.append(fileContents);
     textEditor.dataset.path = filePath;
+    editorContainer.append(editorHeader, textEditor);
+    openTerminal(filename, fileContents);
     textEditor.focus();
   } catch (error) {
     console.log(error);
   }
 }
 
+/**
+ * @type {HTMLDivElement | HTMLIFrameElement}
+ */
+let terminal;
+/**
+ *
+ * @param {string} filename
+ * @param {string} fileContents
+ */
+async function openTerminal(filename, fileContents) {
+  if (terminal) {
+    clearElementsChildren(terminal);
+  }
+  if (filename.split(".")[1] === "js") {
+    terminal = document.createElement("div");
+    terminal.className = "terminal h-50 w-100";
+    runTerminal();
+  } else {
+    terminal = document.createElement("iframe");
+    terminal.srcdoc = fileContents;
+    terminal.style.display = "block";
+    terminal.className = "terminal h-50 w-100";
+  }
+  editorContainer.append(terminal);
+}
+
+function runTerminal() {
+  if (terminal.tagName === "DIV") {
+    terminal.innerText =
+      JSON.stringify(eval(textEditor.innerText.replace(/console.log/g, ""))) ||
+      null;
+  } else {
+    terminal.srcdoc = textEditor.innerText;
+  }
+}
 async function saveFileContents() {
   try {
     await addFileContents(
       textEditor.dataset.path,
       textEditor.innerHTML.replace(/&lt;/g, "<").replace(/&gt;/g, ">")
     );
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 }
