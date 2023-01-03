1b1680a305757f41c8b534d2ce77f6307f151eeb
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2023-01-03 23:00:15 +0200

    environment plugin

--- client/webpack.prod.config.js
+++ client/webpack.prod.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const webpack = require('webpack')
 
 module.exports = {
   mode: "production",
@@ -26,5 +26,6 @@ module.exports = {
       template: "./src/index.html",
     }),
     new MiniCssExtractPlugin(),
+    new webpack.EnvironmentPlugin(),
   ],
 };
