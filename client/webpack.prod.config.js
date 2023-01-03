801851f1152b7655b859d62f31841493bf82855f
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2023-01-03 22:11:03 +0200

    webpack.prod.config

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "./src/app/"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    assetModuleFilename: "icons/[name][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Code Editor",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
